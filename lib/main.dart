5370d5dca18dbf3c97ef98d153a5095f1562ee82
AndrewChacon <andrew.b.chacon@gmail.com>
2019-12-25 23:37:04 -0500

    pokemon details display

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 import 'package:pokemon_app/pokemon.dart';
+import 'package:pokemon_app/pokemondetail.dart';
 
 void main() => runApp(MaterialApp(
   title: 'Poke App',
@@ -12,7 +12,7 @@ void main() => runApp(MaterialApp(
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomePageState createState()  => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
@@ -31,9 +31,11 @@ class _HomePageState extends State<HomePage> {
     var res = await http.get(url);
     // print(res.body);
     var decodedJson = jsonDecode(res.body);
-
     pokeHub = PokeHub.fromJson(decodedJson);
-
+    print(pokeHub.toJson());
+    setState(() {
+       
+    });
   }
 
   @override
@@ -43,22 +45,33 @@ class _HomePageState extends State<HomePage> {
         title: new Text('Poke App'),
         backgroundColor: Colors.cyan,
       ),
-      body: GridView.count(crossAxisCount: 2, children: pokeHub.pokemon.map((poke)=> Padding(
+      body: pokeHub == null? Center(child: CircularProgressIndicator(),):
+       GridView.count(crossAxisCount: 2, children: pokeHub.pokemon.map((poke)=> Padding(
         padding: const EdgeInsets.all(2.0),
-        child: Card(
-          elevation: 3.0,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: NetworkImage(poke.img))
-                ),
+        child: InkWell(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>PokeDetail(
+              pokemon: poke,
+            )));
+          },
+          child: Hero(
+            tag:  poke.img,
+            child: Card(
+              elevation: 3.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(image: NetworkImage(poke.img))
+                    ),
+                  ),
+                  Text(poke.name, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))
+                ],
               ),
-              Text(poke.name, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold))
-            ],
+            ),
           ),
         ),
       )).toList(),),
